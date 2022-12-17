1f221a48108007a6f01b400b6f3b541902972bb0
Hyper-terminal <87844081+Hyper-terminal@users.noreply.github.com>
2022-12-17 12:20:34 +0530

    increment/decrement added

--- react-redux/src/components/Counter.js
+++ react-redux/src/components/Counter.js
@@ -1,15 +1,33 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
+import { useDispatch, useSelector } from "react-redux";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+    const counterDispatcher = useDispatch();
+    const counter = useSelector((state) => state.counter);
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+    const toggleCounterHandler = () => {};
+
+    const incrementHandler = () => {
+        counterDispatcher({ type: "INCREMENT" });
+    };
+
+    const decrementHandler = () => {
+        counterDispatcher({ type: "DECREMENT" });
+    };
+
+    return (
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            <div className={classes.value}>{counter}</div>
+
+            <div>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={decrementHandler}>Decrement</button>
+            </div>
+
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+    );
 };
 
 export default Counter;
--- react-redux/src/store/index.js
+++ react-redux/src/store/index.js
@@ -1,16 +1,12 @@
 import { createStore } from "redux";
 
-const defaultVal = {
-    counter: 0,
-};
-
-const counterReducer = (state = defaultVal, action) => {
+const counterReducer = (state = { counter: 0 }, action) => {
     if (action.type === "INCREMENT") {
-        return state.counter + 1;
+        return { counter: state.counter + 1 };
     }
 
     if (action.type === "DECREMENT") {
-        return state.counter - 1;
+        return { counter: state.counter - 1 };
     }
 
     return state;
